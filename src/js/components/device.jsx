ca065ee46a593e5a135df60b5b64f25674c442e5
Adam Edgett <AdamEdgett@gmail.com>
2015-01-27 21:03:23 -0500

    FIx up React props
    Add PropType definitions

--- src/js/components/device.jsx
+++ src/js/components/device.jsx
@@ -1,10 +1,27 @@
 var React = require('react')
 
+const DEVICE_TYPES = [
+  'outlet',
+  'gas-valve',
+  'gas-sensor',
+  'lock'
+];
+
 var Device = React.createClass({
+  propTypes: {
+    on: React.PropTypes.bool.isRequired,
+    type: React.PropTypes.oneOf(DEVICE_TYPES).isRequired,
+    name: React.PropTypes.string.isRequired
+  },
+
   render: function() {
+    const state = this.props.on ? 'on' : 'off';
     return (
-      <div className={this.props.state + " device-outer-circle"}>
-        {this.props.state}
+      <div className="device">
+        <div className="device-name">{this.props.name}</div>
+        <div className={state + " device-outer-circle"}>
+          {state}
+        </div>
       </div>
     );
   }
--- src/js/components/page.jsx
+++ src/js/components/page.jsx
@@ -1,14 +1,35 @@
 var React = require('react')
 var Device = require('./device.jsx')
+var _ = require('underscore')
 
 var Page = React.createClass({
+  propTypes: {
+    devices: React.PropTypes.arrayOf(React.PropTypes.object),
+    name: React.PropTypes.string
+  },
+
+  getDefaultProps: function(){
+    return {
+      devices: [
+        { on: true, type: "outlet", name: "Kitchen Outlet" },
+        { on: false, type: "lock", name: "Cabinet" }
+      ]
+    };
+  },
+
   render: function(){
+    var renderedDevices = _.map(this.props.devices, this.renderDevice);
+
     return (
     <div>
       <h1>Hello, {this.props.name}!</h1>
-      <Device state='on'/>
+      {renderedDevices}
     </div>
     );
+  },
+
+  renderDevice: function(device){
+    return <Device on={device.on} type={device.type} name={device.name} />;
   }
 });
 
